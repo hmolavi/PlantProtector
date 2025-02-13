a5eef0d381cddb6cd54ccd2cd380ef0cfeb403db
hmolavi <hosseinmolavi907@gmail.com>
2025-02-13 01:16:31 -0500

    console working via components, wifi still working

--- components/library/common.h
+++ components/library/common.h
@@ -10,7 +10,7 @@
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
-#define DEFAULT_SSID "BELL993"
-#define DEFAULT_PASS "F6C21AD5"
+#define DEFAULT_SSID "BELL"
+#define DEFAULT_PASS "BELL"
 
 #endif // __COMMON_H__
---
+++ components/library/parameters.h
@@ -0,0 +1,19 @@
+
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+#include <stdio.h>
+#include <string.h>
+#include "nvs_flash.h"
+#include "nvs.h"
+
+
+// void load_parameters();
+// void save_parameters();
+// void set_param1(int value);
+// void set_param2(float value);
+// void set_param3(const char *value);
+// void schedule_save();
+
+
+#endif // __PARAMETERS_H__
