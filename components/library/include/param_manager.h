bdf1ba2b9a4414e66439c761581108902b86ac16
Hossein Molavi <hmolavi@uwaterloo.ca>
2025-02-16 21:46:19 -0500

    Enhance parameter management with improved getter/setter functions and add support for array handling

--- components/library/include/param_manager.h
+++ components/library/include/param_manager.h
@@ -3,6 +3,10 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "esp_err.h"
+
+#define ARRAY_INIT(...) {__VA_ARGS__}
+
 ///@brief Program variable parameters. Format is
 ///       (type, variable name, default value, description,
 ///       PascalName). For arrays, you must set the max
@@ -10,8 +14,9 @@
 #define PARAMETER_TABLE                                           \
     PARAM(int32_t, brightness, 50, "brightness duh", Brightness)  \
     PARAM(uint32_t, interval, 1000, "random interval", Internval) \
-    ARRAY(char, 64, ssid, "", "WiFi ssid", Ssid)                  \
-    ARRAY(char, 64, password, "", "WiFi password", Password)
+    ARRAY(char, 32, ssid, "", "WiFi ssid", Ssid)                  \
+    ARRAY(char, 64, password, "", "WiFi password", Password)      \
+    ARRAY(int, 4, myarray, ARRAY_INIT(1, 0, 0, 0), "example int array", MyArray)
 
 #define PARAM(type_, name_, default_value_, description_, pn) \
     struct {                                                  \
@@ -26,7 +31,7 @@
     struct {                                                         \
         const char* name;                                            \
         type_ value[size_];                                          \
-        const uint32_t size;                                         \
+        const size_t size;                                           \
         bool dirty;                                                  \
         const type_ default_value[size_];                            \
         const char* description;                                     \
@@ -43,15 +48,29 @@ struct ParamMasterControl {
 ///        flash, they will be set to default value. Global variable
 extern struct ParamMasterControl g_params;
 
-// Getter and Setters
-#define PARAM(type, name, default, description, pn) \
-    void Param_Set##pn(const type value);           \
-    type Param_Get##pn(void);
-#define ARRAY(type, size, name, default_value, description, pn) \
-    void Param_Set##pn(const type* value);                      \
-    type* Param_Get##pn(void);
+// Getter and Setters for truly immutable access and no memory-tracking-hassle!
+///@brief PARAM Getters, Setters, and Resetters are all trivial.
+///       ARRAY Getters return a pointer to a constant of the appropriate type.
+///       ARRAY Setters perform bound checks before setting the value.
+///       ARRAY Resetters work as expected.
+///       ARRAY Copy allows copying the array to a provided buffer.
+#define PARAM(type_, name_, default_value_, description_, pn) \
+    esp_err_t Param_Set##pn(const type_ value);               \
+    type_ Param_Get##pn(void);                                \
+    esp_err_t Param_Reset##pn(void);
+#define ARRAY(type_, size_, name_, default_value_, description_, pn) \
+    esp_err_t Param_Set##pn(const type_* value, size_t length);      \
+    const type_* Param_Get##pn(size_t* out_length);                  \
+    esp_err_t Param_Copy##pn(type_* buffer, size_t buffer_size);     \
+    esp_err_t Param_Reset##pn(void);
 PARAMETER_TABLE
 #undef PARAM
 #undef ARRAY
 
-void param_manager_init(void);
+/// @brief Attempts to pull g_params from nvs flash, if value failed or non-existant,
+///        value will be set to default and dirty flag will be set to true. Also
+///        creates periodic timer for nvs parameter saves; set to 30 seconds
+void Param_ManagerInit(void);
+
+/// @brief Identifies parameters that have been modified (dirty) and saves them to nvs
+void Param_SaveDirtyParameters(void);
--- components/library/param_manager.c
+++ components/library/param_manager.c
@@ -2,19 +2,24 @@
 #include "include/param_manager.h"
 
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
 #include "nvs.h"
 #include "nvs_flash.h"
 
 static const char* TAG = "param_manager.c";
 
+#define NVS_NAMESPACE "param_storage"
+
 ///@brief Parameter storage initialization. Cant set the default
 ///       here because arrays need to be strncpy. Will be done in
-///       param_manager_init()
+///       Param_ManagerInit()
 ///       Stringify the name for the key with #name_
 #define PARAM(type_, name_, default_value_, description_, pn) \
     .name_ = {                                                \
@@ -40,39 +45,67 @@ struct ParamMasterControl g_params = {
 
 ///@brief Getters and Setters
 ///
-#define PARAM(type, name, default, description, pn) \
-    void Param_Set##pn(const type value)            \
-    {                                               \
-        if (g_params.name.value != value) {         \
-            g_params.name.value = value;            \
-            g_params.name.dirty = true;             \
-        }                                           \
-    }                                               \
-    type Param_Get##pn(void)                        \
-    {                                               \
-        return g_params.name.value;                 \
+#define PARAM(type_, name_, default_value_, description_, pn) \
+    esp_err_t Param_Set##pn(const type_ value)                \
+    {                                                         \
+        if (g_params.name_.value != value) {                  \
+            g_params.name_.value = value;                     \
+            g_params.name_.dirty = true;                      \
+            return ESP_OK;                                    \
+        }                                                     \
+        return ESP_FAIL;                                      \
+    }                                                         \
+    type_ Param_Get##pn(void)                                 \
+    {                                                         \
+        return g_params.name_.value;                          \
+    }                                                         \
+    esp_err_t Param_Reset##pn(void)                           \
+    {                                                         \
+        g_params.name_.value = default_value_;                \
+        return ESP_OK;                                        \
     }
-#define ARRAY(type, size, name, default, description, pn)                    \
-    void Param_Set##pn(const type* value)                                    \
-    {                                                                        \
-        if (memcmp(&g_params.name.value, value, size * sizeof(type)) != 0) { \
-            memcpy(&g_params.name.value, value, size * sizeof(type));        \
-            g_params.name.dirty = true;                                      \
-        }                                                                    \
-    }                                                                        \
-    type* Param_Get##pn(void)                                                \
-    {                                                                        \
-        return g_params.name.value;                                          \
+#define ARRAY(type_, size_, name_, default_value_, description_, pn)                         \
+    esp_err_t Param_Set##pn(const type_* value, size_t length)                               \
+    {                                                                                        \
+        if (length > size_) {                                                                \
+            return ESP_ERR_INVALID_SIZE;                                                     \
+        }                                                                                    \
+        if (memcmp(&g_params.name_.value, value, length * sizeof(type_)) != 0) {             \
+            memcpy(&g_params.name_.value, value, length * sizeof(type_));                    \
+            g_params.name_.dirty = true;                                                     \
+            return ESP_OK;                                                                   \
+        }                                                                                    \
+        return ESP_ERR_INVALID_ARG;                                                          \
+    }                                                                                        \
+    const type_* Param_Get##pn(size_t* out_length)                                           \
+    {                                                                                        \
+        if (out_length) *out_length = g_params.name_.size;                                   \
+        return g_params.name_.value;                                                         \
+    }                                                                                        \
+    esp_err_t Param_Copy##pn(type_* buffer, size_t buffer_size)                              \
+    {                                                                                        \
+        const size_t required_size = g_params.name_.size * sizeof(type_);                    \
+        if (buffer_size < required_size) {                                                   \
+            return ESP_ERR_INVALID_SIZE;                                                     \
+        }                                                                                    \
+        memcpy(buffer, g_params.name_.value, required_size);                                 \
+        return ESP_OK;                                                                       \
+    }                                                                                        \
+    esp_err_t Param_Reset##pn(void)                                                          \
+    {                                                                                        \
+        memcpy(&g_params.name_.value, &g_params.name_.default_value, size_ * sizeof(type_)); \
+        return ESP_OK;                                                                       \
     }
 PARAMETER_TABLE
 #undef PARAM
 #undef ARRAY
 
-static void save_dirty_parameters(void)
+/// @brief Identifies parameters that have been modified (dirty) and saves them to nvs
+void Param_SaveDirtyParameters(void)
 {
     nvs_handle_t handle;
     esp_err_t err;
-    if (nvs_open("storage", NVS_READWRITE, &handle) != ESP_OK) return;
+    if (nvs_open(NVS_NAMESPACE, NVS_READWRITE, &handle) != ESP_OK) return;
     int parametersChanged;
     parametersChanged = 0;
 
@@ -80,9 +113,7 @@ static void save_dirty_parameters(void)
     if (g_params.name_.dirty) {                                                                      \
         size_t name_##required_size = sizeof(g_params.name_.value);                                  \
         err = nvs_set_blob(handle, g_params.name_.key, &g_params.name_.value, name_##required_size); \
-        if (err != ESP_OK) {                                                                         \
-            ESP_LOGE(TAG, "Failed to set blob for: %s\n", g_params.name_.name);                      \
-        }                                                                                            \
+        if (err != ESP_OK) ESP_LOGE(TAG, "Failed to set blob for: %s\n", g_params.name_.name);       \
         g_params.name_.dirty = false;                                                                \
         parametersChanged++;                                                                         \
     }
@@ -90,9 +121,7 @@ static void save_dirty_parameters(void)
     if (g_params.name_.dirty) {                                                                      \
         size_t name_##required_size = sizeof(g_params.name_.value);                                  \
         err = nvs_set_blob(handle, g_params.name_.key, &g_params.name_.value, name_##required_size); \
-        if (err != ESP_OK) {                                                                         \
-            ESP_LOGE(TAG, "Failed to set blob for: %s\n", g_params.name_.name);                      \
-        }                                                                                            \
+        if (err != ESP_OK) ESP_LOGE(TAG, "Failed to set blob for: %s\n", g_params.name_.name);       \
         g_params.name_.dirty = false;                                                                \
         parametersChanged++;                                                                         \
     }
@@ -107,10 +136,15 @@ static void save_dirty_parameters(void)
     nvs_close(handle);
 }
 
+static void save_dirty_parameters_callback(TimerHandle_t xTimer)
+{
+    Param_SaveDirtyParameters();
+}
+
 /// @brief Attempt to pull g_params from nvs flash, if value failed or non-existant,
 ///        value will be set to default and dirty flag will be set to true. Also
 ///        creates periodic timer for nvs parameter saves; set to 30 seconds
-void param_manager_init(void)
+void Param_ManagerInit(void)
 {
     // NVS initialization
     esp_err_t ret = nvs_flash_init();
@@ -120,24 +154,24 @@ void param_manager_init(void)
     }
 
     nvs_handle_t handle;
-    if (nvs_open("storage", NVS_READWRITE, &handle) == ESP_OK) {
-#define PARAM(type_, name_, default_value_, description_, pn)                                               \
-    size_t name_##required_size = sizeof(g_params.name_.value);                                             \
-    if (nvs_get_blob(handle, g_params.name_.key, &g_params.name_.value, &name_##required_size) != ESP_OK) { \
-        g_params.name_.value = g_params.name_.default_value;                                                \
-        g_params.name_.dirty = true;                                                                        \
-    }                                                                                                       \
-    else {                                                                                                  \
-        g_params.name_.dirty = false;                                                                       \
+    if (nvs_open(NVS_NAMESPACE, NVS_READWRITE, &handle) == ESP_OK) {
+#define PARAM(type_, name_, default_value_, description_, pn)                                                \
+    size_t name_##_required_size = sizeof(g_params.name_.value);                                             \
+    if (nvs_get_blob(handle, g_params.name_.key, &g_params.name_.value, &name_##_required_size) != ESP_OK) { \
+        g_params.name_.value = g_params.name_.default_value;                                                 \
+        g_params.name_.dirty = true;                                                                         \
+    }                                                                                                        \
+    else {                                                                                                   \
+        g_params.name_.dirty = false;                                                                        \
     }
-#define ARRAY(type_, size_, name_, default_value_, description_, pn)                                        \
-    size_t name_##required_size = sizeof(g_params.name_.value);                                             \
-    if (nvs_get_blob(handle, g_params.name_.key, &g_params.name_.value, &name_##required_size) != ESP_OK) { \
-        memcpy(&g_params.name_.value, &g_params.name_.default_value, size_ * sizeof(type_));                \
-        g_params.name_.dirty = true;                                                                        \
-    }                                                                                                       \
-    else {                                                                                                  \
-        g_params.name_.dirty = false;                                                                       \
+#define ARRAY(type_, size_, name_, default_value_, description_, pn)                                         \
+    size_t name_##_required_size = sizeof(g_params.name_.value);                                             \
+    if (nvs_get_blob(handle, g_params.name_.key, &g_params.name_.value, &name_##_required_size) != ESP_OK) { \
+        memcpy(&g_params.name_.value, &g_params.name_.default_value, sizeof(g_params.name_.value));          \
+        g_params.name_.dirty = true;                                                                         \
+    }                                                                                                        \
+    else {                                                                                                   \
+        g_params.name_.dirty = false;                                                                        \
     }
 
         PARAMETER_TABLE
@@ -148,10 +182,17 @@ void param_manager_init(void)
     }
 
     // Periodic save timer
-    static esp_timer_handle_t timer;
-    const esp_timer_create_args_t timer_args = {
-        .callback = (esp_timer_cb_t)save_dirty_parameters,
-        .name = "g_param_save"};
-    esp_timer_create(&timer_args, &timer);
-    esp_timer_start_periodic(timer, 30 * 1000000);  // 30 seconds
+    TimerHandle_t xTimer = xTimerCreate(
+        "g_param_save",
+        pdMS_TO_TICKS(30000),  // Timer period in ticks (30 seconds)
+        pdTRUE,
+        (void*)0,
+        save_dirty_parameters_callback);
+
+    if (xTimer != NULL) {
+        xTimerStart(xTimer, 0);
+    }
+    else {
+        ESP_LOGE(TAG, "Failed to create periodic timer");
+    }
 }
